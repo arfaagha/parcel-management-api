3317b1b32237c66c3d784ba7915eb37c3909289e
AD0380 <arfa.saif92@gmail.com>
2024-01-01 06:35:18 +0200

    add basic test cases with parcel service mocked

--- src/parcel/parcel.controller.spec.ts
+++ src/parcel/parcel.controller.spec.ts
@@ -1,15 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ParcelController } from './parcel.controller';
+import { ParcelDto } from './dto/parcel.dto';
+import { ParcelService } from './parcel.service';
 
 describe('ParcelController', () => {
-  let controller: ParcelController;
 
-  beforeEach(async () => {
+  let controller: ParcelController;
+  let mockService : ParcelService;
+
+
+  //#region Mocking
+  const mockParcelDto: ParcelDto = {
+    sku: "1111-11111111-1111", 
+    description: "jest mocked description", 
+    streetAddress: "la la land", 
+    town: "la la land", 
+    country: "la la land",
+    track: "fak3T7acK3r",
+    deliveryDate: new Date()
+  };
+
+  const mockParcelService: ParcelService = {
+    create: jest.fn((parcel: ParcelDto)=>{
+      return{
+        default:{
+          mockParcelDto
+      },
+    };
+    }),
+
+    get: jest.fn(()=>{
+      return [{...mockParcelDto}];
+    }),
+
+    getBySku: jest.fn((sku: string)=>{
+      return{
+        mockParcelDto
+      };
+    })
+  };
+//#endregion
+  
+beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ParcelController],
-    }).compile();
+      providers: [ParcelService],
+    }).overrideProvider(ParcelService).useValue(mockParcelService).compile();
 
     controller = module.get<ParcelController>(ParcelController);
+    mockService = module.get<ParcelService>(ParcelService);
   });
 
   it('should be defined', () => {
@@ -27,4 +66,17 @@ describe('ParcelController', () => {
   it('should have a getBySku function', ()=>{
     expect(typeof controller.getBySku).toBe('function');
   });
+
+  it('should create a parcel and return the newly created parcel',()=>{
+    expect(controller.create(mockParcelDto)).not.toBeNull();
+  });
+
+  it('should return a list of parcels when get is called',()=>{
+    expect(controller.get()).not.toBeNull();
+  });
+
+  it('should return a parcel with specified SKU',()=>{
+    expect(controller.getBySku('1111-11111111-1111')).not.toBeNull();
+  });
+
 });
