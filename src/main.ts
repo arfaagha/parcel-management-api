9c1bde073b758b6281538e3970880bf8616015b8
AD0380 <arfa.saif92@gmail.com>
2024-01-01 16:07:12 +0200

    add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+ 
+  //swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Parcels')
+    .setDescription('The parcels API description')
+    .setVersion('1.0')
+    .addTag('parcel')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+ 
+
   await app.listen(3000);
 }
 bootstrap();
