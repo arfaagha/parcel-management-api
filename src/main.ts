cce41b2ebb216d0fa8842eec840af6de730dc47d
AD0380 <arfa.saif92@gmail.com>
2024-01-04 01:25:06 +0200

    add enable-cors

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
  
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
