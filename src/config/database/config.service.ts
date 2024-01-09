6398e5a073e027165c93ce24c7424b24281d0348
AD0380 <arfa.saif92@gmail.com>
2024-01-10 00:46:30 +0200

    update: move database configuration to environment variables

--- src/config/database/config.service.ts
+++ src/config/database/config.service.ts
@@ -7,17 +7,17 @@ import { Parcel } from 'src/parcel/entities/parcel.entity';
 
 @Injectable()
 export class PostgresConfigService implements TypeOrmOptionsFactory {
-    constructor(private configService: ConfigService){}
+    constructor(private configService: ConfigService){
+    }
 
-    //this can all be moved to a environment file as well
     createTypeOrmOptions(): TypeOrmModuleOptions{
         return{
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'testuser',
-            database: 'parcel_management',
+            host: this.configService.get<string>('HOST'),
+            port: this.configService.get<number>('PORT'),
+            username: this.configService.get<string>('USER_NAME'),
+            password: this.configService.get<string>('PASSWORD'),
+            database: this.configService.get<string>('DATABASE'),
             synchronize: true,
             autoLoadEntities: true,
             entities: [Parcel],
